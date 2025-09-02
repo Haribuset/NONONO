fb5af5eb108e85072d4a322684edf2e24b23cb42
Shiro Kyaa <darrenwijaya59@gmail.com>
2025-09-02 20:13:46 +0700

    Kalkulator

--- kitaDownload.c
+++ kitaDownload.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
 
-int main(){
-    printf("hello world");
+int main() {
+
+    // Calcuator Progtam
+
+    char operator = '\0';
+    double num1 = 0.0;
+    double num2 = 0.0;
+    double result = 0.0;
+
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the operator ( + - * / ): ");
+    scanf(" %c", &operator);
+
+    printf("Enter tHe second number: ");
+    scanf("%lf", &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+        break;
+        case '-':
+            result = num1 - num2;
+        break;
+        case '*':
+             result = num1 * num2;
+        break;
+        case '/':
+            if(num2 == 0) {
+                printf("You cant divide by zero\n");
+            } else {
+                result = num1 / num2;
+            }
+        break; 
+    }
+
+    printf("Result: %.4lf", result);
+
+    return 0;
 }
